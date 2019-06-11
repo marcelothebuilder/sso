248e332827fdd07d4e618136d6a26f347f1c4a90
Marcelo Paixão Resende <marcelothebuilder@users.noreply.github.com>
2019-06-11 15:19:20 -0300

    adds api endpoint

--- src/app.py
+++ src/app.py
@@ -29,13 +29,14 @@ app.config.update({
         "web-origins"
     ],
     'OIDC_CLIENT_SECRETS': 'client_secrets.json',
-    'OIDC_ID_TOKEN_COOKIE_SECURE': False, # MUST be True in production
-    'OIDC_REQUIRE_VERIFIED_EMAIL': False, # SHOULD be True in production
+    'OIDC_ID_TOKEN_COOKIE_SECURE': False,  # MUST be True in production
+    'OIDC_REQUIRE_VERIFIED_EMAIL': False,  # SHOULD be True in production
     'OIDC_VALID_ISSUERS': ['http://localhost:8080/auth/realms/{}'.format(OPENID_REALM)],
     'OIDC_OPENID_REALM': OPENID_REALM,
     'OIDC_INTROSPECTION_AUTH_METHOD': 'client_secret_post'
 })
 oidc = OpenIDConnect(app)
+
 
 def api_requires_login(fn):
     @functools.wraps(fn)
@@ -50,17 +51,19 @@ def api_requires_login(fn):
 
     return with_api_requires_auth
 
+
 @app.route('/login')
 def login():
     redirect_url = request.args.get('redirect_url', None) or request.url
     return oidc.redirect_to_auth_server(redirect_url)
+
 
 @app.route('/')
 def hello_world():
     if oidc.user_loggedin:
         return ('Hello, %s, <a href="/private">See private</a> '
                 '<a href="/logout">Log out</a>') % \
-            oidc.user_getfield('email')
+               oidc.user_getfield('email')
     else:
         return 'Welcome anonymous, <a href="/private">Log in</a>'
 
@@ -68,18 +71,34 @@ def hello_world():
 @app.route('/private')
 @api_requires_login
 def hello_me():
-    info = oidc.user_getinfo(["aud", # user realm
-        "sub", # the user id
-        "auth_time",
-        "name", # full name
-        "given_name", # first name
-        "family_name", # last name
-        "preferred_username", #username
-        "email" # email
-        ])
-    print(info)
+    info = oidc.user_getinfo(["aud",  # user realm
+                              "sub",  # the user id
+                              "auth_time",
+                              "name",  # full name
+                              "given_name",  # first name
+                              "family_name",  # last name
+                              "preferred_username",  # username
+                              "email"  # email
+                              ])
+
     return ('Hello, %s (%s)! <a href="/">Return</a>' %
             (info.get('email'), info.get('sub')))
+
+
+@app.route('/private_api')
+@api_requires_login
+def hello_me_api():
+    info = oidc.user_getinfo(["aud",  # user realm
+                              "sub",  # the user id
+                              "auth_time",
+                              "name",  # full name
+                              "given_name",  # first name
+                              "family_name",  # last name
+                              "preferred_username",  # username
+                              "email"  # email
+                              ])
+
+    return flask.jsonify(info)
 
 
 @app.route('/api')
