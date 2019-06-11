962cd194a95af7248edf3879cad7654109f10390
Marcelo Paixão Resende <marcelothebuilder@users.noreply.github.com>
2019-06-11 15:14:40 -0300

    adds spa login

--- src/app.py
+++ src/app.py
@@ -1,12 +1,10 @@
+import functools
 import json
 
 import flask
-from flask import Flask, render_template, g
 import flask_login
-from flask_login import login_required
+from flask import Flask, g, request, url_for
 from flask_oidc import OpenIDConnect
-
-from model.user import User
 
 OPENID_REALM = 'flask-test'
 
@@ -34,10 +32,28 @@ app.config.update({
     'OIDC_ID_TOKEN_COOKIE_SECURE': False, # MUST be True in production
     'OIDC_REQUIRE_VERIFIED_EMAIL': False, # SHOULD be True in production
     'OIDC_VALID_ISSUERS': ['http://localhost:8080/auth/realms/{}'.format(OPENID_REALM)],
-    'OIDC_OPENID_REALM': 'http://localhost:5000/oidc_callback',
+    'OIDC_OPENID_REALM': OPENID_REALM,
     'OIDC_INTROSPECTION_AUTH_METHOD': 'client_secret_post'
 })
 oidc = OpenIDConnect(app)
+
+def api_requires_login(fn):
+    @functools.wraps(fn)
+    def with_api_requires_auth(*args, **kwargs):
+        if oidc.user_loggedin:
+            return fn(*args, **kwargs)
+
+        response = flask.jsonify({'error': 'This resource required authentication.'})
+        response.status_code = 401
+        response.headers.add('WWW-Authenticate', url_for('login', redirect_url=request.url))
+        return response
+
+    return with_api_requires_auth
+
+@app.route('/login')
+def login():
+    redirect_url = request.args.get('redirect_url', None) or request.url
+    return oidc.redirect_to_auth_server(redirect_url)
 
 @app.route('/')
 def hello_world():
@@ -50,7 +66,7 @@ def hello_world():
 
 
 @app.route('/private')
-@oidc.require_login
+@api_requires_login
 def hello_me():
     info = oidc.user_getinfo(["aud", # user realm
         "sub", # the user id
